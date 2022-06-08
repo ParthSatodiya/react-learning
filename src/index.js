4a9abe33cddff1a8fca0bef4c82785199492c000
Parth Satodiya <parth.satodiya@noovosoft.com>
2022-06-08 06:49:21 +0000

    Add basic tic-tac-toe components and interactions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board/>
+                </div>
+                <div className='game-info'>
+                    <div>{/*status*/}</div>
+                    <ol>{/*TODO*/}</ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    renderSquare(i) {
+        return (<Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleCLick(i)}
+        />);
+    }
+
+    handleCLick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    render() {
+        const status = 'Next Player: -';
+        return (
+            <div>
+                <div className='status'>
+                    {status}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button 
+                className='square'
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Game/>)
