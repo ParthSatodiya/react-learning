20890c208937fe253caeae56540d1fd7db6e0fde
Parth Satodiya <parth.satodiya@noovosoft.com>
2022-06-08 07:24:21 +0000

    Add user turns and declaring winner

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
 
@@ -34,13 +35,23 @@ class Board extends React.Component {
     }
 
     handleCLick(i) {
+        if (calculateWinner(this.state.squares) || this.state.squares[i]) {
+            return;
+        }
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({squares: squares, xIsNext: !this.state.xIsNext});
     }
 
     render() {
-        const status = 'Next Player: -';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        
         return (
             <div>
                 <div className='status'>
@@ -82,13 +93,33 @@ class Board extends React.Component {
 function Square(props) {
     return (
         <button 
-                className='square'
-                onClick={props.onClick}
-            >
-                {props.value}
-            </button>
+            className='square'
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
     );
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
